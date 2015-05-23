dd8f4312057609dec99ce6aeafb903239a68eea2
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2015-05-23 22:37:23 +0200

    Move Msg to separate file

--- logger.go
+++ logger.go
@@ -18,13 +18,11 @@ import (
 	"io"
 	"os"
 	"runtime"
-	"strings"
 	"time"
 )
 
 const (
 	defaultStackSize  = 4096
-	defaultMsgSize    = 100
 	defaultLogsSize   = 1024
 	defaultErrorsSize = 10
 )
@@ -50,77 +48,6 @@ type fileWriter struct {
 // Close calls Close on the underlying os.File.
 func (w *fileWriter) Close() error {
 	return w.f.Close()
-}
-
-// Msg is a message created by a log operation.
-type Msg struct {
-	Level, Msg string
-	Tags       Tags
-	Timestamp  time.Time
-}
-
-// String creates a string message in the following format:
-//	YYYY-MM-DD HH:MM:SS [LEVEL] tag1, tag2: message
-func (msg *Msg) String() string {
-	return string(msg.Bytes())
-}
-
-// Bytes formats a message in the following format:
-//	YYYY-MM-DD HH:MM:SS [LEVEL] tag1, tag2: message
-func (msg *Msg) Bytes() []byte {
-	buf := make([]byte, 0, defaultMsgSize)
-
-	// Write the date and time.
-	// Format: "YYYY-MM-DD HH:MM:SS ".
-	year, month, day := msg.Timestamp.Date()
-	hour, min, sec := msg.Timestamp.Clock()
-	itoa(&buf, year, 4)
-	buf = append(buf, '-')
-	itoa(&buf, int(month), 2)
-	buf = append(buf, '-')
-	itoa(&buf, day, 2)
-	buf = append(buf, ' ')
-	itoa(&buf, hour, 2)
-	buf = append(buf, ':')
-	itoa(&buf, min, 2)
-	buf = append(buf, ':')
-	itoa(&buf, sec, 2)
-	buf = append(buf, ' ')
-
-	// Write the level.
-	// Format: "[LEVEL] " (level is always 5 characters long).
-	buf = append(buf, '[')
-	buf = append(buf, msg.Level...)
-	buf = append(buf, ']')
-	buf = append(buf, ' ')
-
-	// Write the tags.
-	// Format: "tag1, tag2: ".
-	buf = append(buf, msg.Tags.Bytes()...)
-	buf = append(buf, ':')
-	buf = append(buf, ' ')
-
-	// The actual message.
-	buf = append(buf, strings.TrimSpace(msg.Msg)...)
-	buf = append(buf, '\n')
-
-	return buf
-}
-
-// Cheap integer to fixed-width decimal ASCII. Modified version from the Golang
-// logger package.
-func itoa(buf *[]byte, i int, wid int) {
-	var b [4]byte // only used for year, month and day so 4 is enough.
-	bp := len(b) - 1
-	for i >= 10 || wid > 1 {
-		wid--
-		q := i / 10
-		b[bp] = byte('0' + i - q*10)
-		bp--
-		i = q
-	}
-	b[bp] = byte('0' + i)
-	*buf = append(*buf, b[bp:]...)
 }
 
 // Collection of all created loggers by name, used by the Get function.
---
+++ msg_test.go
@@ -0,0 +1,79 @@
+// Copyright (C) 2015 Thomas de Zeeuw.
+//
+// Licensed onder the MIT license that can be found in the LICENSE file.
+
+package logger
+
+import (
+	"testing"
+	"time"
+)
+
+func TestItoa(t *testing.T) {
+	tests := []struct {
+		input    int
+		width    int
+		expected string
+	}{
+		{1999, 4, "1999"}, // In case of time travel.
+		{2009, 4, "2009"},
+		{2010, 4, "2010"},
+		{2015, 4, "2015"},
+		{2016, 4, "2016"},
+		{2017, 4, "2017"},
+		{2018, 4, "2018"},
+		{2019, 4, "2019"},
+		{2020, 4, "2020"},
+		{5000, 4, "5000"}, // At this point, all bets are off.
+		{1, 2, "01"},
+		{5, 2, "05"},
+		{8, 2, "08"},
+		{10, 2, "10"},
+		{11, 2, "11"},
+		{12, 2, "12"},
+		{21, 2, "21"},
+		{25, 2, "25"},
+		{31, 2, "31"},
+	}
+
+	for _, test := range tests {
+		var buf []byte
+		itoa(&buf, test.input, test.width)
+
+		if got := string(buf); got != test.expected {
+			t.Errorf("Expected itoa(%v, %d, %d) to return %q, but got %q",
+				&buf, test.input, test.width, test.expected, got)
+		}
+	}
+}
+
+func TestMsg(t *testing.T) {
+	var now = time.Now()
+	var tStr = now.Format("2006-01-02 15:04:05")
+
+	var msgTests = []struct {
+		msg      Msg
+		expected string
+	}{
+		{Msg{"FATAL", "Message", Tags{}, now},
+			tStr + " [FATAL] : Message\n"},
+		{Msg{"ERROR", "Message", Tags{"tag1"}, now},
+			tStr + " [ERROR] tag1: Message\n"},
+		{Msg{"INFO ", "Message", Tags{"tag1", "tag2"}, now},
+			tStr + " [INFO ] tag1, tag2: Message\n"},
+		{Msg{"DEBUG", "Message", Tags{"tag1", "tag2", "tag3"}, now},
+			tStr + " [DEBUG] tag1, tag2, tag3: Message\n"},
+	}
+
+	for _, test := range msgTests {
+		got, gotB := test.msg.String(), test.msg.Bytes()
+
+		if got != string(gotB) {
+			t.Errorf("Msg.Bytes() and String() don't return the same value, got %q"+
+				" and %q, want %q", got, string(gotB), test.expected)
+		} else if got != test.expected {
+			t.Errorf("Expected Msg.String() to return %q, got %q",
+				test.expected, got)
+		}
+	}
+}
