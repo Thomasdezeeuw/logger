6cc7ebaeadb15d0df6db7ac73cdf23850de9ac27
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2015-07-15 16:42:51 +0200

    Add NewWriter

--- writer.go
+++ writer.go
@@ -18,12 +18,16 @@ func (iw *ioWriterMsgWriter) Close() error {
 	return nil
 }
 
+// NewWriter creates a new logger that writes to the given io.Writer.
+func NewWriter(name string, w io.Writer) (*Logger, error) {
+	mw := &ioWriterMsgWriter{w}
+	return New(name, mw)
+}
+
 // Error ouput, usefull for testing.
 var stderr io.Writer = os.Stderr
 
 // NewConsole creates a new logger that writes to error output (os.Stderr).
 func NewConsole(name string) (*Logger, error) {
-	mw := &ioWriterMsgWriter{stderr}
-	return New(name, mw)
+	return NewWriter(name, stderr)
 }
-
