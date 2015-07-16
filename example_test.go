6073e811312e6cff2816607d172d62dcc0bd4987
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2015-07-16 17:44:15 +0200

    Add examples
    Add examples for Logger.Warn Logger.Thumbstone and Logger.Close.
    
    Also for NewConsole, NewFile, NewWriter and Combine.

--- example_test.go
+++ example_test.go
@@ -5,8 +5,11 @@
 package logger
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
+	"os"
+	"path/filepath"
 	"time"
 )
 
@@ -104,8 +107,19 @@ func ExampleLogger_Error() {
 
 	err = errors.New("Some error")
 	log.Error(Tags{"file.go", "main"}, err)
-	// Logs some like:
+	// Logs:
 	// 2015-03-01 17:20:52 [Error] file.go, main: Some error
+}
+
+func ExampleLogger_Warn() {
+	log, err := NewConsole("App")
+	if err != nil {
+		panic(err)
+	}
+
+	log.Warn(Tags{"file.go", "main"}, "my %s message", "warning")
+	// Logs:
+	// 2015-03-01 17:20:52 [Warn] file.go, main: My warning message
 }
 
 func ExampleLogger_Info() {
@@ -128,6 +142,108 @@ func ExampleLogger_Debug() {
 	log.Debug(Tags{"file.go", "main"}, "my %s message", "debug")
 	// Logs:
 	// 2015-03-01 17:20:52 [Debug] file.go, main: My debug message
+}
+
+func ExampleLogger_Thumbstone() {
+	log, err := NewConsole("App")
+	if err != nil {
+		panic(err)
+	}
+
+	var myMaybeUnusedFunction = func() bool {
+		tags := Tags{"example_test.go", "myMaybeUnusedFunction"}
+		log.Thumbstone(tags, "Still alive")
+
+		return false
+	}
+
+	myMaybeUnusedFunction()
+
+	// Logs:
+	// 2015-03-01 17:20:52 [Thumb] example_test.go, myMaybeUnusedFunction: Still alive
+}
+
+func ExampleLogger_Close() {
+	log, err := NewConsole("App")
+	if err != nil {
+		panic(err)
+	}
+
+	// Make sure we close the logger. This call will block until all the logs are
+	// written. After calling Logger.Close and calls to a log operation
+	// (Logger.Fatal, .Error etc.) will panic.
+	defer log.Close()
+
+	log.Info(Tags{"file.go", "main"}, "my %s message", "info")
+	// Logs to stderr:
+	// 2015-03-01 17:20:52 [Info] file.go, main: My info message
+}
+
+func ExampleNewConsole() {
+	log, err := NewConsole("App")
+	if err != nil {
+		panic(err)
+	}
+
+	log.Info(Tags{"file.go", "main"}, "my %s message", "info")
+	// Logs to stderr:
+	// 2015-03-01 17:20:52 [Info] file.go, main: My info message
+}
+
+func ExampleNewFile() {
+	filepath := filepath.Join(os.TempDir(), "Logger.log")
+	log, err := NewFile("App", filepath)
+	if err != nil {
+		panic(err)
+	}
+
+	log.Info(Tags{"file.go", "main"}, "my %s message", "info")
+	// Logs to file:
+	// 2015-03-01 17:20:52 [Info] file.go, main: My info message
+}
+
+func ExampleCombine() {
+	var Production bool
+
+	filepath := filepath.Join(os.TempDir(), "Logger.log")
+	fileLog, err := NewFile("FileLog", filepath)
+	if err != nil {
+		panic(err)
+	}
+	logs := []*Logger{fileLog}
+
+	// When not in production, we could the output on our console, so we'll
+	// create a console logger.
+	if !Production {
+		consoleLog, err := NewConsole("ConsoleLog")
+		if err != nil {
+			panic(err)
+		}
+		logs = append(logs, consoleLog)
+	}
+
+	// Now we combine the loggers, in we production only log to the file, but in
+	// development we also logs to the console.
+	log, err := Combine("App", logs...)
+	if err != nil {
+		panic(err)
+	}
+
+	log.Info(Tags{"file.go", "main"}, "my %s message", "info")
+	// Logs to stderr and/or the file:
+	// 2015-03-01 17:20:52 [Info] file.go, main: My info message
+}
+
+func ExampleNewWriter() {
+	var buf bytes.Buffer
+	log, err := NewWriter("App", &buf)
+	if err != nil {
+		panic(err)
+	}
+
+	log.Info(Tags{"file.go", "main"}, "my %s message", "info")
+	// Logs to the buffer:
+	// 2015-03-01 17:20:52 [Info] file.go, main: My info message
 }
 
 func ExampleGet() {
