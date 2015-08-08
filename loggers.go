b89b5a6d07e66ea30801c15f628691b5356c74f1
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2015-08-08 18:55:38 +0200

    Move sepcial loggers to own file
    This commit creates a new loggers.go file and move all the special
    loggers, like 'file', 'writer' and 'console', to it. This makes it
    easier to create new loggers, without creating a new file for each on.

--- loggers.go
+++ loggers.go
@@ -2,6 +2,7 @@ package logger
 
 import (
 	"bufio"
+	"encoding/json"
 	"io"
 	"os"
 )
@@ -46,3 +47,37 @@ func NewFile(name, path string) (*Logger, error) {
 	mw := &fileMsgWriter{bufio.NewWriter(f), f}
 	return New(name, mw)
 }
+
+type ioWriterMsgWriter struct {
+	w io.Writer
+}
+
+func (iw *ioWriterMsgWriter) Write(msg Msg) error {
+	bytes := append(msg.Bytes(), '\n')
+	n, err := iw.w.Write(bytes)
+	if err != nil {
+		return err
+	} else if n != len(bytes) {
+		return io.ErrShortWrite
+	}
+	return nil
+}
+
+func (iw *ioWriterMsgWriter) Close() error {
+	return nil
+}
+
+// NewWriter creates a new logger that writes to the given io.Writer.
+func NewWriter(name string, w io.Writer) (*Logger, error) {
+	mw := &ioWriterMsgWriter{w}
+	return New(name, mw)
+}
+
+// Error ouput, usefull for testing.
+var stderr io.Writer = os.Stderr
+
+// NewConsole creates a new logger that writes to error output (os.Stderr).
+func NewConsole(name string) (*Logger, error) {
+	return NewWriter(name, stderr)
+}
+
