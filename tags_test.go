4626be1921027e24b690e1bcc0c50028e13cb6c0
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2015-09-03 14:47:26 +0200

    Add tests for Tags.MarshalJSON

--- tags_test.go
+++ tags_test.go
@@ -10,22 +10,31 @@ func TestTags(t *testing.T) {
 	t.Parallel()
 
 	var tagTests = []struct {
-		tags     Tags
-		expected string
+		tags         Tags
+		expected     string
+		expectedJSON string
 	}{
-		{Tags{}, ""},
-		{Tags{"tag1"}, "tag1"},
-		{Tags{"tag1", "tag2"}, "tag1, tag2"},
-		{Tags{"tag1", "tag2", "tag3"}, "tag1, tag2, tag3"},
+		{Tags{}, "", "[]"},
+		{Tags{"tag1"}, "tag1", `["tag1"]`},
+		{Tags{"tag1", "tag2"}, "tag1, tag2", `["tag1", "tag2"]`},
+		{Tags{"tag1", "tag2", "tag3"}, "tag1, tag2, tag3", `["tag1", "tag2", "tag3"]`},
 	}
 
 	for _, test := range tagTests {
 		got, gotBytes := test.tags.String(), string(test.tags.Bytes())
-
 		if gotBytes != test.expected {
-			t.Error(compareError("Tags%v.Bytes()", test.tags, test.expected, gotBytes))
+			t.Errorf("Expected Tags{%s}.Bytes() to return %q, but got %q",
+				test.tags.String(), test.expected, gotBytes)
 		} else if got != test.expected {
-			t.Error(compareError("Tags%v.String()", test.tags, test.expected, got))
+			t.Errorf("Expected Tags{%s}.String() to return %q, but got %q",
+				test.tags.String(), test.expected, got)
+		}
+
+		if json, err := test.tags.MarshalJSON(); err != nil {
+			t.Errorf("Unexpected error marshaling %v into json: %s", test.tags, err.Error())
+		} else if got := string(json); got != test.expectedJSON {
+			t.Errorf("Expected Tags{%s}.MarshalJSON() to return %q, but got %q",
+				test.tags.String(), test.expectedJSON, got)
 		}
 	}
 }
