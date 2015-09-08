6391e687f7c83286f618c16bcfe580b57f471e38
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2015-09-08 13:58:50 +0200

    Add Event
    This replaces Msg in logging.

---
+++ event.go
@@ -0,0 +1,133 @@
+// Copyright (C) 2015 Thomas de Zeeuw.
+//
+// Licensed onder the MIT license that can be found in the LICENSE file.
+
+package logger
+
+// todo: benchmark Event, EventType, Tags.String/Bytes with reference and not.
+
+import (
+	"fmt"
+	"math"
+	"strconv"
+	"time"
+)
+
+// TimeFormat is used in Event.String() and Event.Bytes() to format the
+// timestamp.
+const TimeFormat = "2006-01-02 15:04:05"
+
+// Event is created by a log operation. The timezone of timestamp is always the
+// current timezone, recommend is to log time in the UTC timezone, by calling
+// Event.Timestamp.UTC(), Event.String() does this by default.
+type Event struct {
+	Type      EventType
+	Timestamp time.Time
+	Tags      Tags
+	Message   string
+	Data      interface{}
+}
+
+// String formats an event in the  following format:
+//	YYYY-MM-DD HH:MM:SS [TYPE] tag1, tag2: message, data
+//
+// Note: the timestamp is set to the UTC timezone.
+//
+// Note: if is data is nil it doesn't get added to the message, so the format
+// wil be:
+//	YYYY-MM-DD HH:MM:SS [TYPE] tag1, tag2: message
+func (event Event) String() string {
+	str := event.Timestamp.UTC().Format(TimeFormat)
+	str += " [" + event.Type.String() + "] "
+	str += event.Tags.String() + ": "
+	str += event.Message
+	if event.Data != nil {
+		str += ", " + interfaceToString(event.Data)
+	}
+	return str
+}
+
+// Bytes does the same as Event.String(), but returns a byte slice.
+func (event Event) Bytes() []byte {
+	return []byte(event.String())
+}
+
+// MarshalJSON coverts the event to a JSON formatted byte slice. It uses
+// time.RFC3339Nano to format the timestamp.
+func (event Event) MarshalJSON() ([]byte, error) {
+	tagsJSON, err := event.Tags.MarshalJSON()
+	if err != nil {
+		return []byte{}, err
+	}
+
+	str := fmt.Sprintf(`{"type": %q, "timestamp": %q, "tags": %s, "message": %q`,
+		event.Type.String(), event.Timestamp.UTC().Format(time.RFC3339Nano),
+		string(tagsJSON), event.Message)
+	if event.Data != nil {
+		str += fmt.Sprintf(`, "data": %q`, interfaceToString(event.Data))
+	}
+	str += "}"
+	return []byte(str), nil
+}
+
+// EventType indicent which level of detail a log operation has.
+type EventType uint16
+
+// Log levels available by default.
+const (
+	DebugEvent EventType = iota
+	InfoEvent
+	WarnEvent
+	ErrorEvent
+	FatalEvent
+	ThumbEvent
+)
+
+var (
+	eventTypeNames   = "DebugInfoWarnErrorFatalThumb"
+	eventTypeIndices = []int{0, 5, 9, 13, 18, 23, 28}
+)
+
+// String returns the name of the event type. Custom event types are also
+// supported, if created with NewEventType.
+func (eventType EventType) String() string {
+	if !isDefinedEventType(eventType) {
+		return fmt.Sprintf("EventType(%d)", eventType)
+	}
+
+	startIndex := eventTypeIndices[eventType]
+	endIndex := eventTypeIndices[eventType+1]
+	return eventTypeNames[startIndex:endIndex]
+}
+
+func isDefinedEventType(eventType EventType) bool {
+	return int(eventType) <= len(eventTypeIndices)-1
+}
+
+// Bytes does the same as EventType.String(), but returns a byte slice.
+func (eventType EventType) Bytes() []byte {
+	return []byte(eventType.String())
+}
+
+func (eventType EventType) MarshalJSON() ([]byte, error) {
+	qoutedEventType := strconv.Quote(eventType.String())
+	return []byte(qoutedEventType), nil
+}
+
+// NewEventType creates a new fully supported custom EventType to be used in
+// logging. This function makes sure that EventType.String() and
+// EventType.Bytes() always return the correct name.
+//
+// Note: THIS FUNCTION IS NOT THREAD SAFE, use it before starting to log.
+//
+// Note: The maximum number of custom log levels is 65528, if more are created
+// this function will panic.
+func NewEventType(name string) EventType {
+	if len(eventTypeIndices) >= math.MaxUint16 {
+		panic("ini: can't have more then 65535 log levels")
+	}
+
+	eventTypeNames += name
+	eventTypeIndices = append(eventTypeIndices, len(eventTypeNames))
+	return EventType(len(eventTypeIndices) - 2)
+}
---
+++ event_test.go
@@ -0,0 +1,169 @@
+// Copyright (C) 2015 Thomas de Zeeuw.
+//
+// Licensed onder the MIT license that can be found in the LICENSE file.
+
+package logger
+
+import (
+	"errors"
+	"fmt"
+	"math"
+	"testing"
+	"time"
+)
+
+type stringer struct{}
+
+func (s *stringer) String() string {
+	return "data"
+}
+
+func TestEvent(t *testing.T) {
+	defer resetEventTypes()
+
+	now := time.Now()
+	tStr := now.UTC().Format(TimeFormat)
+	tStrNano := now.UTC().Format(time.RFC3339Nano)
+
+	var msgTests = []struct {
+		event        Event
+		expected     string
+		expectedJSON string
+	}{
+		{Event{FatalEvent, now, Tags{}, "Message1", nil},
+			tStr + " [Fatal] : Message1",
+			`{"type": "Fatal", "timestamp": "` + tStrNano + `", "tags": [], ` +
+				`"message": "Message1"}`},
+		{Event{ErrorEvent, now, Tags{"tag1"}, "Message2", "data"},
+			tStr + " [Error] tag1: Message2, data",
+			`{"type": "Error", "timestamp": "` + tStrNano + `", "tags": ["tag1"], ` +
+				`"message": "Message2", "data": "data"}`},
+		{Event{WarnEvent, now, Tags{"tag1"}, "Message3", &stringer{}},
+			tStr + " [Warn] tag1: Message3, data",
+			`{"type": "Warn", "timestamp": "` + tStrNano + `", "tags": ["tag1"], ` +
+				`"message": "Message3", "data": "data"}`},
+		{Event{InfoEvent, now, Tags{"tag1", "tag2"}, "Message4", []byte("data")},
+			tStr + " [Info] tag1, tag2: Message4, data",
+			`{"type": "Info", "timestamp": "` + tStrNano + `", "tags": ["tag1", "tag2"], ` +
+				`"message": "Message4", "data": "data"}`},
+		{Event{ThumbEvent, now, Tags{"tag1", "tag2", "tag3"}, "Message5", errors.New("error data")},
+			tStr + " [Thumb] tag1, tag2, tag3: Message5, error data",
+			`{"type": "Thumb", "timestamp": "` + tStrNano + `", "tags": ["tag1", "tag2", "tag3"], ` +
+				`"message": "Message5", "data": "error data"}`},
+		{Event{DebugEvent, now, Tags{"tag1", "tag2", "tag3"}, "Message6", 0},
+			tStr + " [Debug] tag1, tag2, tag3: Message6, 0",
+			`{"type": "Debug", "timestamp": "` + tStrNano + `", "tags": ["tag1", "tag2", "tag3"], ` +
+				`"message": "Message6", "data": "0"}`},
+		{Event{NewEventType("My-event-type"), now, Tags{"tag1"}, "Message7", nil},
+			tStr + " [My-event-type] tag1: Message7",
+			`{"type": "My-event-type", "timestamp": "` + tStrNano + `", "tags": ["tag1"], ` +
+				`"message": "Message7"}`},
+		{Event{NewEventType(`my-"event"-type`), now, Tags{`tag"1"`}, "Message7", `"`},
+			tStr + " [my-\"event\"-type] tag\"1\": Message7, \"",
+			`{"type": "my-\"event\"-type", "timestamp": "` + tStrNano + `", "tags": ["tag\"1\""], ` +
+				`"message": "Message7", "data": "\""}`},
+	}
+
+	for _, test := range msgTests {
+		got, gotBytes := test.event.String(), string(test.event.Bytes())
+		if gotBytes != test.expected {
+			t.Errorf("Expected Event(%v).Bytes() to return %q, but got %q",
+				test.event, test.expected, gotBytes)
+		} else if got != test.expected {
+			t.Errorf("Expected Event(%v).String() to return %q, but got %q",
+				test.event, test.expected, got)
+		}
+
+		if json, err := test.event.MarshalJSON(); err != nil {
+			t.Errorf("Unexpected error marshaling %v into json: %s", test.event, err.Error())
+		} else if got := string(json); got != test.expectedJSON {
+			t.Errorf("Expected Event(%v).MarshalJSON() to return %q, but got %q",
+				test.event, test.expectedJSON, got)
+		}
+	}
+}
+
+func TestEventType(t *testing.T) {
+	defer resetEventTypes()
+
+	tests := []struct {
+		eventType    EventType
+		expected     string
+		expectedJSON string
+	}{
+		{DebugEvent, "Debug", `"Debug"`},
+		{ThumbEvent, "Thumb", `"Thumb"`},
+		{InfoEvent, "Info", `"Info"`},
+		{WarnEvent, "Warn", `"Warn"`},
+		{ErrorEvent, "Error", `"Error"`},
+		{FatalEvent, "Fatal", `"Fatal"`},
+		{EventType(255), "EventType(255)", `"EventType(255)"`},
+		{NewEventType("my-event-type"), "my-event-type", `"my-event-type"`},
+		{NewEventType("my-\"event\"-type"), "my-\"event\"-type", `"my-\"event\"-type"`},
+	}
+
+	for _, test := range tests {
+		got, gotBytes := test.eventType.String(), string(test.eventType.Bytes())
+		if gotBytes != test.expected {
+			t.Errorf("Expected EventType(%v).Bytes() to return %q, but got %q",
+				test.eventType, test.expected, gotBytes)
+		} else if got != test.expected {
+			t.Errorf("Expected EventType(%v).String() to return %q, but got %q",
+				test.eventType, test.expected, got)
+		}
+
+		if json, err := test.eventType.MarshalJSON(); err != nil {
+			t.Errorf("Unexpected error marshaling %v into json: %s", test.eventType, err.Error())
+		} else if got := string(json); got != test.expectedJSON {
+			t.Errorf("Expected EventType(%v).MarshalJSON() to return %q, but got %q",
+				test.eventType, test.expectedJSON, got)
+		}
+	}
+}
+
+func TestNewLogLevelLimit(t *testing.T) {
+	t.Skip("TestNewLogLevelLimit takes almost 6 seconds, far to long")
+	if testing.Short() {
+		t.Skip("Skipping TestNewLogLevelLimit in short mode")
+	}
+
+	defer resetEventTypes()
+	const max = math.MaxUint16 - 7 // minus builtin event types.
+
+	for i := 1; i <= max; i++ {
+		expected := fmt.Sprintf("EventType-%d", i)
+		eventType := NewEventType(expected)
+		if got := eventType.String(); got != expected {
+			t.Fatalf("Expected NewEventType(%q).String() to return %q, but got %q",
+				expected, expected, got)
+		}
+	}
+
+	defer func() {
+		recv := recover()
+		if recv == nil {
+			t.Fatal("Expected a panic after creating 65528 log levels, but didn't get one")
+		}
+
+		got, ok := recv.(string)
+		if !ok {
+			t.Fatalf("Expected the recoverd panic to be a string, but it's %v", recv)
+		}
+
+		expected := "ini: can't have more then 65535 log levels"
+		if got != expected {
+			t.Fatalf("Expected the recoverd panic to be %s, but got %s", expected, got)
+		}
+	}()
+	NewEventType(fmt.Sprintf("EventType-%d", max+1))
+}
+
+var (
+	oldEventTypeNames   = eventTypeNames
+	oldEventTypeIndices = eventTypeIndices
+)
+
+func resetEventTypes() {
+	eventTypeNames = oldEventTypeNames
+	eventTypeIndices = oldEventTypeIndices
+}
