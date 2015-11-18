5f9f418c5d55e6392b0f984aa60977452e8eb3a4
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2015-11-18 22:24:18 +0100

    Test logToEvent with errors

--- log_bridge_test.go
+++ log_bridge_test.go
@@ -60,3 +60,43 @@ func TestBridgeLogPgk(t *testing.T) {
 	log.Println("Log message newline")
 	log.Panic("Panic message")
 }
+
+func TestLogToEventError(t *testing.T) {
+	defer reset()
+
+	tags := Tags{"TestLogToEventError"}
+	w := logToEvent{tags, time.Now().Location()}
+
+	t1 := now()
+	line1 := "otherPrefix:2015/11/18 22:07:20.284275\n"
+	w.Write([]byte(line1))
+	line2 := logPrefix + "\n"
+	w.Write([]byte(line2))
+	line3 := logPrefix + "2015/18/11 22:07:20.284275\n"
+	w.Write([]byte(line3)) // yyyy/dd/mm
+	close(eventChannel)
+
+	expected := []Event{
+		{Type: ErrorEvent, Timestamp: t1, Tags: tags, Message: ErrLogFormat.Error(), Data: line1},
+		{Type: ErrorEvent, Timestamp: t1, Tags: tags, Message: ErrLogFormat.Error(), Data: line2},
+		{Type: ErrorEvent, Timestamp: t1, Tags: tags, Message: "parsing time \"2015/18/11 22:07:20.284275\": month out of range", Data: line3},
+	}
+
+	const margin = 100 * time.Millisecond
+	i := 0
+	for event := range eventChannel {
+		expectedEvent := expected[i]
+		i++
+
+		// Can't mock time in the log package, so we have a truncate it.
+		if !event.Timestamp.Truncate(margin).Equal(expectedEvent.Timestamp.Truncate(margin)) {
+			t.Errorf("Expected event #%d to be %v, but got %v", i, expectedEvent, event)
+			continue
+		}
+		event.Timestamp = expectedEvent.Timestamp
+
+		if expected, got := expectedEvent, event; !reflect.DeepEqual(expected, got) {
+			t.Errorf("Expected event #%d to be %v, but got %v", i, expected, got)
+		}
+	}
+}
