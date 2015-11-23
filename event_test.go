30f223523fb2d874bb9e3f903ad1ec090d0e7b2d
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2015-11-23 20:05:52 +0100

    Fix type-o in TestNewEventTypeWithEmptyName

--- event_test.go
+++ event_test.go
@@ -125,13 +125,13 @@ func TestNewEventTypeWithEmptyName(t *testing.T) {
 	defer func() {
 		recv := recover()
 		if recv == nil {
-			t.Fatal(`Expceted NewEventType("") to panic, but it didn't`)
+			t.Fatal(`Expected NewEventType("") to panic, but it didn't`)
 		}
 
 		expected := "logger: EventType name can't be empty"
 		got := recv.(string)
 		if got != expected {
-			t.Fatalf("Expceted panic value to be %s, but got %s", expected, got)
+			t.Fatalf("Expected panic value to be %s, but got %s", expected, got)
 		}
 	}()
 
